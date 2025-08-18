d45dfd3af513e3b655f65c0c77e32662bc3726f2
Peter Petraník <peter.petranik@bitmovin.com>
2025-08-18 12:22:44 +0200

    Add tailwind
    - with `astro add tailwind` according to https://docs.astro.build/en/guides/styling/#tailwind
    - use global.css instead of GlobalStyles.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from "astro/config";
 import devtoolsJson from "vite-plugin-devtools-json";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     // Example: Add custom vite plugins directly to your Astro project
-    plugins: [devtoolsJson()],
+    plugins: [devtoolsJson(), tailwindcss()],
   },
 });

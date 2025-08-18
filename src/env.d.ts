2abf368fec66a4f16b96f6f40d57eeb0d83e97df
Peter Petraník <peter.petranik@bitmovin.com>
2025-08-18 12:06:38 +0200

    Add page: program.astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,7 @@
 type PrimaryLocale = "en";
 type SecondaryLocale = "sr";
 type Locale = PrimaryLocale | SecondaryLocale;
-type RouteParameters = { "/en": undefined };
+type RouteParameters = { "/en": undefined; "/en/program": undefined };
 type Route = keyof RouteParameters;
 type TranslationVariables = {
   title: object | undefined;

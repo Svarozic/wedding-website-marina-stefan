52bc0fca9284965fa9038e775f529e66092e0653
Peter Petraník <peter.petranik@bitmovin.com>
2025-08-18 09:36:54 +0200

    feat: i18n Astro recipe
    https://docs.astro.build/en/recipes/i18n/

---
+++ src/i18n/ui.ts
@@ -0,0 +1,16 @@
+export const languages = {
+  en: "English",
+  sr: "Srpski",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+  sr: {
+    "nav.home": "Početna",
+  },
+} as const;
